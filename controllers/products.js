a85a86ed50861c0d5781ad4349aa5cc415e28e9c
hanh301084 <nha302031@gmail.com>
2020-01-01 17:19:58 +0700

    view, pagination comment

--- controllers/products.js
+++ controllers/products.js
@@ -42,7 +42,6 @@ exports.product_sort_price=async (req, res) => {
     try {
         const page = req.params.page;
         const products = await Products.find().sort({"price":1}).skip(page * 8).limit(8);
-        //res.json(posts);
         res.render('products', {
             title1: '', title2: '', title3: 'selected', title4: '', title5: '', title6: '', title7: '',
             Products: products, i: 0,
@@ -61,13 +60,13 @@ exports.product_detail=async (req, res) => {
         const id = req.params.id;
         const products = await Products.findById(id);
         const productsOther= await Products.find({"type":products.type});
-        const listComment= await Comments.find({"id_product":id});
+        const listComment= await Comments.find({"id_product":id}).skip(req.params.page_comment*5).limit(5);
         //res.json(posts);
         res.render('productdetail', {
             title1: '', title2: '', title3: 'selected', title4: '', title5: '', title6: '', title7: '',
             Products: products, i: 0,
             ProductsOther: productsOther,
-            ListComment: listComment,
+            ListComment: listComment, page_comment_current: req.params.page_comment*1, j:0,
             user: req.user
         });
     } catch (err) {
@@ -102,7 +101,7 @@ exports.post_comment= async (req, res) => {
     });
     try {
         const saveCment = await cment.save()
-        res.redirect('/products/product_detail');  //trả về trang hiện tại
+        res.redirect('/products/0');  //trả về trang hiện tại
     } catch (err) {
         res.json({ message: err });
     }
--- routes/products.js
+++ routes/products.js
@@ -8,9 +8,9 @@ router.get('/sort_price/:page',controller.product_sort_price);
 
 router.get('/sort_name/:page',controller.product_sort_name);
 
-router.get('/product_detail/:id',controller.product_detail);
-
 router.post('/product_detail/:id/add_comment',controller.post_comment);
+
+router.get('/product_detail/:id/:page_comment',controller.product_detail);
 
 router.get('/:type/:page',controller.product_type);
 
